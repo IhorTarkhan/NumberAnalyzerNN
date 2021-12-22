55b0f3ce6267666dab8a913bc46d97962baa58e3
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2021-12-22 23:43:29 +0200

    code formatted

--- src/main/java/com/example/number/analyzer/Layer.java
+++ src/main/java/com/example/number/analyzer/Layer.java
@@ -1,17 +1,15 @@
 package com.example.number.analyzer;
 
 public class Layer {
+  public int size;
+  public double[] neurons;
+  public double[] biases;
+  public double[][] weights;
 
-    public int size;
-    public double[] neurons;
-    public double[] biases;
-    public double[][] weights;
-
-    public Layer(int size, int nextSize) {
-        this.size = size;
-        neurons = new double[size];
-        biases = new double[size];
-        weights = new double[size][nextSize];
-    }
-
+  public Layer(int size, int nextSize) {
+    this.size = size;
+    neurons = new double[size];
+    biases = new double[size];
+    weights = new double[size][nextSize];
+  }
 }
