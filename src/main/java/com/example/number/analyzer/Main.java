957fe3adffdfcc536a00cfde5d28cc7b2ba019cb
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2021-12-23 11:34:40 +0200

    writen to file init logs

--- src/main/java/com/example/number/analyzer/Main.java
+++ src/main/java/com/example/number/analyzer/Main.java
@@ -8,22 +8,28 @@ import java.util.Objects;
 import java.util.function.UnaryOperator;
 
 public class Main {
+  public static final int EPOCHS = 1000;
+  public static final int BATCH_SIZE = 100;
+
   public static void main(String[] args) throws IOException {
     UnaryOperator<Double> sigmoid = x -> 1 / (1 + Math.exp(-x));
-    UnaryOperator<Double> dsigmoid = y -> y * (1 - y);
-    NeuralNetwork nn = new NeuralNetwork(0.001, sigmoid, dsigmoid, 784, 512, 128, 32, 10);
+    UnaryOperator<Double> dSigmoid = y -> y * (1 - y);
+    NeuralNetwork nn = new NeuralNetwork(0.001, sigmoid, dSigmoid, 784, 512, 128, 32, 10);
 
     File[] imagesFiles = Objects.requireNonNull(new File("./train").listFiles());
-    int samples = imagesFiles.length;
-    BufferedImage[] images = new BufferedImage[samples];
-    int[] digits = new int[samples];
-    for (int i = 0; i < imagesFiles.length; i++) {
+
+    BufferedImage[] images = new BufferedImage[imagesFiles.length];
+    for (int i = 0; i < images.length; i++) {
       images[i] = ImageIO.read(imagesFiles[i]);
+    }
+
+    int[] digits = new int[imagesFiles.length];
+    for (int i = 0; i < imagesFiles.length; i++) {
       digits[i] = Integer.parseInt(imagesFiles[i].getName().charAt(10) + "");
     }
 
-    double[][] inputs = new double[samples][784];
-    for (int i = 0; i < samples; i++) {
+    double[][] inputs = new double[images.length][784];
+    for (int i = 0; i < images.length; i++) {
       for (int x = 0; x < 28; x++) {
         for (int y = 0; y < 28; y++) {
           inputs[i][x + y * 28] = (images[i].getRGB(x, y) & 0xff) / 255.0;
@@ -31,13 +37,11 @@ public class Main {
       }
     }
 
-    int epochs = 10;
-    for (int i = 1; i < epochs; i++) {
+    for (int i = 1; i < EPOCHS; i++) {
       int right = 0;
       double errorSum = 0;
-      int batchSize = 100;
-      for (int j = 0; j < batchSize; j++) {
-        int imgIndex = (int) (Math.random() * samples);
+      for (int j = 0; j < BATCH_SIZE; j++) {
+        int imgIndex = (int) (Math.random() * images.length);
         double[] targets = new double[10];
         int digit = digits[imgIndex];
         targets[digit] = 1;
